348bf47d8cec44ddfc9b9688b85aa426ff218db6
0x1a8510f2 <admin@0x1a8510f2.space>
2021-06-19 23:04:16 +0100

    Check result in `ping` command

--- src/cmds/ping.rs
+++ src/cmds/ping.rs
@@ -1,5 +1,7 @@
 // Check if the bot is online
 
+use locales::t;
+
 pub struct Ping {}
 impl Ping { pub const fn new() -> Self { Self {} } }
 
@@ -19,5 +21,10 @@ impl crate::cmds::Command for Ping {
         let result = ctx.room.unwrap().send(matrix_sdk::events::AnyMessageEventContent::RoomMessage(matrix_sdk::events::room::message::MessageEventContent::notice_plain(
             "Pong 🏓",
         )), None).await;
+
+        match result {
+            Ok(_) => {},
+            Err(msg) => ctx.logger.log(crate::log::LogLevel::Error, t!("err.matrix.event_send", err: &msg.to_string(), ctx.lang)),
+        }
     }
 }
